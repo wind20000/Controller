af448a15b6229ae5e46c98cabcb9cae49010dc86
wind-of-the-city <842936992@qq.com>
2018-08-23 16:47:18 +0800

    define open

--- LPDEC-04D01H_V01.01_180521/Code/3 App/config.h
+++ LPDEC-04D01H_V01.01_180521/Code/3 App/config.h
@@ -3,13 +3,13 @@
 
 
 
-#define DEBUG_CONTROLLER            (0)   /*打印LOG*/ 
+#define DEBUG_CONTROLLER            (1)   /*打印LOG*/ 
 
-#define DEBUG_RTY_FUN               (0)   /*1: 重传功能测试使能0: 关闭测试功能*/
+#define DEBUG_RTY_FUN               (1)   /*1: 重传功能测试使能0: 关闭测试功能*/
 
-#define DEBUG_TROPE_FUN             (0)   /*1: 测试转义使能,0:测试转义失能*/
+#define DEBUG_TROPE_FUN             (1)   /*1: 测试转义使能,0:测试转义失能*/
 
-#define CONFIG_RETRY_COPY           (0)   /*1: 重传时直接COPY包数据，而不是重新组包，0:失能此功能*/
+#define CONFIG_RETRY_COPY           (1)   /*1: 重传时直接COPY包数据，而不是重新组包，0:失能此功能*/
 
 #endif
 
